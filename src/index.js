bfa74535c8788cd4ec1884a36f7b2a6c74d040fa
David Salas <dsalas@digitalgeko.com>
2019-08-12 11:10:57 -0600

    Added code until v. 13

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import './global.css'
-import Badge from './components/Badge'
+import BadgeNew from './pages/BadgeNew'
 
 const container = document.getElementById('app')
 
-ReactDOM.render(<Badge
-  firstName='José'
-  lastName='Morales'
-  jobTitle='Frontend Engineer'
-  twitter='Iron_Chepe'
-  avatarUrl='https://s.gravatar.com/avatar/d33af9e22f1fa895dac14f62c2644505?s=360' />,
-container)
+ReactDOM.render(<BadgeNew />,
+  container)
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import './styles/BadgeNew.css'
+import header from '../images/badge-header.svg'
+import Navbar from '../components/Navbar'
+import Badge from '../components/Badge'
+
+class BadgeNew extends React.Component {
+  render () {
+    return (
+      <div>
+        <Navbar />
+        <div className='BadgeNew__hero'>
+          <img className='img-fluid' src={header} alt='Logo' />
+        </div>
+        <div className='container'>
+          <div className='row'>
+            <div className='col'>
+              <Badge firstName='José'
+                lastName='Morales'
+                jobTitle='Salesforce Developer'
+                twitter='Iron_Chepe'
+                avatarUrl='https://s.gravatar.com/avatar/d33af9e22f1fa895dac14f62c2644505?s=360' />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BadgeNew
