28c6211f769d11f62f4290ed3a1e6b53815ccd16
David Salas <jlmruiz@outlook.com>
2019-08-16 14:46:23 -0600

    Added code until v. 32

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -3,92 +3,51 @@ import { Link } from 'react-router-dom'
 
 import './styles/BadgeDetails.css'
 import confLog from '../images/platziconf-logo.svg'
-import PageLoading from '../components/PageLoading'
-import PageError from '../components/PageError'
 import Badge from '../components/Badge'
-import api from '../api'
 
-class BadgeDetails extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      loading: true,
-      error: null,
-      data: undefined
-    }
-  }
+function BadgeDetails (props) {
+  const { id, firstName, lastName, email, avatarUrl, twitter, jobTitle } = props.badge
 
-  componentDidMount () {
-    this.fetchData()
-  }
-
-  fetchData = async () => {
-    this.setState({ loading: true, error: null })
-
-    try {
-      const data = await api.badges.read(this.props.match.params.badgeId)
-      this.setState({ loading: false, data })
-    } catch (error) {
-      this.setState({ loading: false, error })
-    }
-  }
-
-  handleChange = () => {
-
-  }
-
-  render () {
-    if (this.state.loading) {
-      return <PageLoading />
-    }
-
-    if (this.state.error) {
-      return <PageError error={this.state.error} />
-    }
-
-    const { id, firstName, lastName, email, avatarUrl, twitter, jobTitle } = this.state.data
-
-    return (
-      <React.Fragment>
-        <div className='BadgeDetails__hero'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-6'>
-                <img src={confLog} alt='Logo de la Conferencia' />
-              </div>
-              <div className='.BadgeDetails__hero-attendant-name  col-6' >
-                <h1>{firstName} {lastName}</h1>
-              </div>
-            </div>
-          </div>
-        </div>
+  return (
+    <React.Fragment>
+      <div className='BadgeDetails__hero'>
         <div className='container'>
           <div className='row'>
-            <div className='col'>
-              <Badge firstName={firstName}
-                lastName={lastName}
-                email={email}
-                jobTitle={jobTitle}
-                avatarUrl={avatarUrl}
-                twitter={twitter}
-                onChange={this.handleChange} />
+            <div className='col-6'>
+              <img src={confLog} alt='Logo de la Conferencia' />
             </div>
-            <div className='col' >
-              <h2>Actions</h2>
-              <div>
-                <Link className='btn btn-primary mb-4' to={`/badges/${id}/edit`} >
-                    Edit
-                </Link>
-              </div>
-              <div>
-                <button className='btn btn-danger'>Delete</button>
-              </div>
+            <div className='.BadgeDetails__hero-attendant-name  col-6' >
+              <h1>{firstName} {lastName}</h1>
             </div>
           </div>
         </div>
-      </React.Fragment>
-    )
-  }
+      </div>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <Badge firstName={firstName}
+              lastName={lastName}
+              email={email}
+              jobTitle={jobTitle}
+              avatarUrl={avatarUrl}
+              twitter={twitter}
+              onChange={this.handleChange} />
+          </div>
+          <div className='col' >
+            <h2>Actions</h2>
+            <div>
+              <Link className='btn btn-primary mb-4' to={`/badges/${id}/edit`} >
+                Edit
+              </Link>
+            </div>
+            <div>
+              <button className='btn btn-danger'>Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default BadgeDetails
---
+++ src/pages/BadgeDetailsContainer.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import PageLoading from '../components/PageLoading'
+import PageError from '../components/PageError'
+import api from '../api'
+import BadgeDetails from './BadgeDetails'
+
+class BadgeDetailsContainer extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loading: true,
+      error: null,
+      data: undefined
+    }
+  }
+
+  componentDidMount () {
+    this.fetchData()
+  }
+
+  fetchData = async () => {
+    this.setState({ loading: true, error: null })
+
+    try {
+      const data = await api.badges.read(this.props.match.params.badgeId)
+      this.setState({ loading: false, data })
+    } catch (error) {
+      this.setState({ loading: false, error })
+    }
+  }
+
+  handleChange = () => {
+
+  }
+
+  render () {
+    if (this.state.loading) {
+      return <PageLoading />
+    }
+
+    if (this.state.error) {
+      return <PageError error={this.state.error} />
+    }
+
+    return (
+      <BadgeDetails badge={this.state.data} />
+    )
+  }
+}
+
+export default BadgeDetailsContainer
